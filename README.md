48413c9aeade03407300a690cb3806773ad6350e
dasein <dshin.42@gmail.com>
2018-12-27 00:49:18 -0800

    functional gestures box for simple clock vs anticlock

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# 3D Mouse Project
+# Gesture Box Project
 
-You would move around a conventional mouse on a table to interact with your personal computer. Here we extend a dimension and build a 3D mouse prototype using a standard 6-axis accel-gyro (MPU6050).
+Build a simple human input device based on 3-axis accelerometer (MPU6050) gesture recognition to interface with machines.
 
 ## Prototype
 
-- Plotter for raw 3D accel and debug use
-- Use 3-axis accel to control 3D, and a shaker for an interaction. Shaker will be based on a low-compute classifier that can run on a MacBook CPU in real-time.
+- Simple Arduino + MPU6050 HW for algo prototyping
+- Keras based model for circle, linear action classification
+- A live debug plotter for 3-axis accel with classification result
---
+++ src/main.py
@@ -0,0 +1,125 @@
+"""Build a simple gestures box by running a simple neural net classifier on 3-axis accel data (MPU6050) stream. 
+
+	Force ~15 Hz stream on ide side
+
+	Data flow:
+		[mpu6050 raw accel]
+		-> feature extraction (e.g. subspacing)
+		-> classification (e.g. NN)
+		-> [inference result (e.g. shaking the box, drawing a circle with the box)]
+	
+	(TODO)
+	Display rate is lower than stream rate and lagging aggregates 
+		- need to put check condition until stream hits most recent value
+
+"""
+
+import numpy as np
+import serial
+import sys
+import time
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.neural_network import MLPClassifier
+
+
+IS_DEBUG = 0 # mode for debug plot and print
+IS_PRINT_FS = 0
+IS_PRINT_STREAM = 0 # mode for printing stream val
+
+N_BUFFER_DISPLAY = 100 # size of buffer for autogain display
+N_BUFFER_COMPUTE = 20 # size of buffer for classifier compute
+
+N_DISPLAY_HEIGHT = 500 # size of image height for display
+
+# fixed parameters.
+BAUDRATE = 9600
+N_DIM = 3   # dimension of input stream
+
+
+def extract_features(M):
+	for k in range(M.shape[1]):
+		M[:, k] -= np.mean(M[:, k])
+	v_feat = M.flatten()
+	return v_feat
+
+def round_clean(val):
+	"""Round to first dec point.
+	"""
+	return int(val * 10) / 10.
+
+def autogain(v):
+	"""Compress a np vector into [0, 1] for display purposes.
+	"""
+	return (v - min(v)) / (max(v) - min(v))
+
+def shift_buffer(M, val):
+	"""Left-shift a buffer for update values.
+	Assume columnwise update.
+	"""
+	if M.shape[0] != len(val):
+		sys.exit('bad dimensions for update')
+	M[:, :-1] = M[:, 1:]
+	M[:, -1] = val
+	return M
+
+def main(argv):
+	if N_BUFFER_DISPLAY < N_BUFFER_COMPUTE:
+		return -1
+	path_device = argv[0]
+	# Open up MPU6050 stream.
+	ser = serial.Serial(path_device)
+	ser.baudrate = BAUDRATE
+	M_buffer_display = np.zeros((N_DIM, N_BUFFER_DISPLAY)) + 10**-12 # epsilon for autogain conditioning.
+
+	counter_frame = 0
+	time_prev = -1
+	time_now = -1
+	frame_rate = -1
+
+	dict_model = np.load('model.npy').item()
+	clf = dict_model['clf']
+
+	plt.figure
+	while True:
+		line = ser.readline().decode('utf-8')
+		accel_line = line.split(', ')
+		vals = np.zeros(N_DIM)
+		for k in range(N_DIM):
+			vals[k] = float(accel_line[k].split('=')[-1])
+		M_buffer_display = shift_buffer(M_buffer_display, vals)
+		M_buffer_compute = M_buffer_display[:, -N_BUFFER_COMPUTE:]
+
+		if IS_DEBUG:
+			# Normalize display data for image plotting.
+			M_buffer_display_norm = M_buffer_display.copy()
+			for k in range(N_DIM):
+				M_buffer_display_norm[k, :] = autogain(M_buffer_display[k, :])
+			# Convert display data into image domain.
+			M_buffer_display_image = np.zeros((N_DISPLAY_HEIGHT, N_BUFFER_DISPLAY))
+			for k in range(N_DIM):
+				for k_buf in range(N_BUFFER_DISPLAY):
+					M_buffer_display_image[-int(M_buffer_display_norm[0, k_buf] * N_DISPLAY_HEIGHT):, k_buf] = 1
+			print(M_buffer_display_norm) # debug raw accel print
+			cv2.imshow('raw stream', M_buffer_display_image)
+			cv2.waitKey(33)
+		if IS_PRINT_FS:
+			print('fs = ' + str(round_clean(frame_rate)) + ' Hz')
+		if counter_frame == 0:
+			time_prev = time.time()
+		else:
+			time_now = time.time()
+			# compute	frame rate
+			frame_rate = counter_frame / float(time_now - time_prev)
+		if IS_PRINT_STREAM:
+			print(str(time.time()) + ':' + str(M_buffer_compute[:, -1]))
+		counter_frame += 1
+		
+		feat_vec = extract_features(M_buffer_compute.T.copy()).reshape(1, -1)
+		class_predict = clf.predict(feat_vec)[0]
+		print(class_predict)
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
+	print('terminated.')
+
